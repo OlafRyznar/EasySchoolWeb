e35794f0b60d59b32d1ef01b6a048f570685ae8f
OlafRyznar <olaf.ryznar.20@gmail.com>
2024-08-14 13:07:00 +0200

    readme

--- frontend/src/components/SecondaryHeader.jsx
+++ frontend/src/components/SecondaryHeader.jsx
@@ -15,15 +15,15 @@ const SecondaryHeader = () => {
     'student': 'For Student',
     'parent': 'For Parent',
     'other': 'Other',
-    'schedule' : 'Schedule',
-    'presence' : 'Presence',
-    'library' : 'Library',
-    'contact' : 'Contact',
-    'mobile-app' : 'Mobile App',
-    'help' : 'Help',
-    'about-us' : 'About Us',
-    'dues' : 'Dues',
-    'exams' : 'Exams'
+    'schedule': 'Schedule',
+    'presence': 'Presence',
+    'library': 'Library',
+    'contact': 'Contact',
+    'mobile-app': 'Mobile App',
+    'help': 'Help',
+    'about-us': 'About Us',
+    'dues': 'Dues',
+    'exams': 'Exams'
     // Dodaj inne strony, jeśli potrzebne
   };
 
@@ -41,7 +41,12 @@ const SecondaryHeader = () => {
       </button>
 
       {/* Logo Section */}
-      <img className="w-[35%] max-w-[150px] h-auto" src={logo1} alt="Logo 1" />
+      <img
+        className="w-[35%] max-w-[150px] h-auto cursor-pointer"
+        src={logo1}
+        alt="Logo 1"
+        onClick={() => navigate('/')} // Przenieś użytkownika na stronę główną
+      />
 
       {/* Page Title */}
       <div className="flex-grow text-[#519bf3] text-5xl font-extrabold font-['Bitter'] text-center mr-40">
